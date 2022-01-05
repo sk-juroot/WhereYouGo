42451b83ed66c4a9c200a2137d1a7ec3431deb28
JuRoot <juraj@grecnar.sk>
2022-01-05 13:49:05 +0100

    hotfix for (#349) password visible in settings
    used AndroidX settings framework ignores `whereyougo_preferences_credentials.xml` value for hiding text as password, InputType is defined on password EditText binding
    
    commit also fixes check for empty username and password on checklogin functionality (previously left as TODO), missing username or password should disable Check login data functionality
    
    fixes #349

--- src/main/java/menion/android/whereyougo/gui/fragments/settings/SettingsCredentialsFragment.java
+++ src/main/java/menion/android/whereyougo/gui/fragments/settings/SettingsCredentialsFragment.java
@@ -2,6 +2,7 @@ package menion.android.whereyougo.gui.fragments.settings;
 
 import android.os.Bundle;
 
+import android.text.InputType;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
@@ -18,24 +19,29 @@ public class SettingsCredentialsFragment extends PreferenceFragmentCompat {
         EditTextPreference gc_username = findPreference(Preferences.getKey(R.string.pref_KEY_S_GC_USERNAME));
         EditTextPreference gc_password = findPreference(Preferences.getKey(R.string.pref_KEY_S_GC_PASSWORD));
         Preference checkLogin = findPreference(Preferences.getKey(R.string.pref_KEY_S_GC_CHECK));
+        if (checkLogin != null) checkLogin.setEnabled(!(Preferences.GC_USERNAME.isEmpty() || Preferences.GC_PASSWORD.isEmpty()));
 
         if (gc_username != null) {
             gc_username.setOnPreferenceChangeListener((preference, o) -> {
                 Preferences.GC_USERNAME = (String) o;
+                if (checkLogin != null) checkLogin.setEnabled(!(Preferences.GC_USERNAME.isEmpty() || Preferences.GC_PASSWORD.isEmpty()));
                 return true;
             });
         }
         if (gc_password != null) {
+            gc_password.setOnBindEditTextListener(editText -> editText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD));
             gc_password.setOnPreferenceChangeListener((preference, o) -> {
                 Preferences.GC_PASSWORD = (String) o;
+                if (checkLogin != null) checkLogin.setEnabled(!(Preferences.GC_USERNAME.isEmpty() || Preferences.GC_PASSWORD.isEmpty()));
                 return true;
             });
         }
         if (checkLogin != null) {
             checkLogin.setOnPreferenceClickListener(preference -> {
-                // TODO: Check for non empty username and password before
-                LoginTask loginTask = new LoginTask(getView(), Preferences.GC_USERNAME, Preferences.GC_PASSWORD);
-                loginTask.execute();
+                if (!Preferences.GC_USERNAME.isEmpty() && !Preferences.GC_PASSWORD.isEmpty()) {
+                    LoginTask loginTask = new LoginTask(requireView(), Preferences.GC_USERNAME, Preferences.GC_PASSWORD);
+                    loginTask.execute();
+                }
                 return true;
             });
         }
